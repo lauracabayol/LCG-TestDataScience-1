86c831d06082c14a4d60fab7f1ccf72b5478ff03
Laura Cabayol Garcia <lauracabayol@lauras-macbook-pro.home>
2024-09-29 23:35:54 +0200

    commit app.py with feature names

--- app.py
+++ app.py
@@ -10,17 +10,18 @@ with open("model.pkl", "rb") as f:  # Adjusted the path to model.pkl
 def predict(feature1, feature2, feature3, feature4, feature5):
     features = np.array([feature1, feature2, feature3, feature4, feature5]).reshape(1, -1)
     prediction = model.predict(features)
+    #add here conversion to penguin type
     return prediction[0]
 
 # Create the Gradio interface
 interface = gr.Interface(
     fn=predict,
     inputs=[
-        gr.inputs.Number(label="Feature 1"),
-        gr.inputs.Number(label="Feature 2"),
-        gr.inputs.Number(label="Feature 3"),
-        gr.inputs.Number(label="Feature 4"),
-        gr.inputs.Number(label="Feature 5")
+        gr.inputs.Number(label="island"),
+        gr.inputs.Number(label="bill_length_mm"),
+        gr.inputs.Number(label="bill_depth_mm"),
+        gr.inputs.Number(label="flipper_length_mm"),
+        gr.inputs.Number(label="body_mass_g")
     ],
     outputs="text",
     title="Penguin Classifier"
@@ -28,5 +29,3 @@ interface = gr.Interface(
 
 if __name__ == "__main__":
     interface.launch()
-
-
